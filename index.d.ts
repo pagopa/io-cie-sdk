29cc1165bbd3d90d61239369f22ec78b2e4c8f6c
Matteo Boschi <mbmaken@gmail.com>
2020-02-25 15:33:57 +0100

    update type definition

--- index.d.ts
+++ index.d.ts
@@ -19,77 +19,78 @@ event: CIEEvent;
 attemptsLeft: number;
 };
 
-declare module "react-native-cie" {
   
-  export default interface CieManager {
-    /**
-     * check if the OS support CIE autentication
-     */
-    hasApiLevelSupport(): Promise<boolean>;
+class CieManager {
+  /**
+   * check if the OS support CIE autentication
+   */
+  hasApiLevelSupport(): Promise<boolean>;
 
-    /**
-     * check if the device has NFC feature
-     */
-    hasNFCFeature(): Promise<boolean>;
+  /**
+   * check if the device has NFC feature
+   */
+  hasNFCFeature(): Promise<boolean>;
 
-    /**
-     * check if the device running the code supports CIE authentication
-     */
-    isCIEAuthenticationSupported(): Promise<boolean>;
+  /**
+   * check if the device running the code supports CIE authentication
+   */
+  isCIEAuthenticationSupported(): Promise<boolean>;
 
-    /**
-     * check if NFC is enabled
-     */
-    isNFCEnabled(): Promise<boolean>;
+  /**
+   * check if NFC is enabled
+   */
+  isNFCEnabled(): Promise<boolean>;
 
-    /**
-     * launch CieID app (https://play.google.com/store/apps/details?id=it.ipzs.cieid&hl=it)
-     * if it's installed. Otherwise the default browser is opened on the app url
-     */
-    launchCieID(): Promise<void>;
+  /**
+   * launch CieID app (https://play.google.com/store/apps/details?id=it.ipzs.cieid&hl=it)
+   * if it's installed. Otherwise the default browser is opened on the app url
+   */
+  launchCieID(): Promise<void>;
 
-    /**
-     * register a callback to receive all Event raised while reading/writing CIE
-     */
-    onEvent(callback: (event: Event) => void): void;
-    /**
-     * opens OS Settings on NFC section
-     */
+  /**
+   * register a callback to receive all Event raised while reading/writing CIE
+   */
+  onEvent(callback: (event: Event) => void): void;
+  /**
+   * opens OS Settings on NFC section
+   */
 
-    openNFCSettings(): Promise<void>;
-    /**
-     * register a callback to receive errors occured while reading/writing CIE
-     */
+  openNFCSettings(): Promise<void>;
+  /**
+   * register a callback to receive errors occured while reading/writing CIE
+   */
 
-    onError(callback: (error: Error) => void): void;
+  onError(callback: (error: Error) => void): void;
 
-    /**
-     * register a callback to receive the success event containing the consent form url
-     */
-    onSuccess(callback: (url: string) => void): void;
+  /**
+   * register a callback to receive the success event containing the consent form url
+   */
+  onSuccess(callback: (url: string) => void): void;
 
-    setAuthenticationUrl(url: string): void;
+  setAuthenticationUrl(url: string): void;
 
-    /**
-     * set the CIE pin. It has to be a 8 length string of 8 digits
-     */
-    setPin(pin: string): Promise<void>;
+  /**
+   * set the CIE pin. It has to be a 8 length string of 8 digits
+   */
+  setPin(pin: string): Promise<void>;
 
-    start(): Promise<void>;
+  start(): Promise<void>;
 
-    /**
-     * command CIE SDK to start reading/writing CIE CARD
-     */
-    startListeningNFC(): Promise<void>;
+  /**
+   * command CIE SDK to start reading/writing CIE CARD
+   */
+  startListeningNFC(): Promise<void>;
 
-    /**
-     * command CIE SDK to stop reading/writing CIE CARD
-     */
-    stopListeningNFC(): Promise<void>;
+  /**
+   * command CIE SDK to stop reading/writing CIE CARD
+   */
+  stopListeningNFC(): Promise<void>;
 
-    /**
-     * Remove all events callbacks: onEvent / onError / onSuccess
-     */
-    removeAllListeners(): void;
-  }
+  /**
+   * Remove all events callbacks: onEvent / onError / onSuccess
+   */
+  removeAllListeners(): void;
 }
+
+const cieManager : CieManager = new CieManager();
+export default cieManager;
