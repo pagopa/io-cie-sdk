7e34220778c225bfe00041b237d823f5d4d14048
GiovanniMancini <g.mancini.0@gmail.com>
2020-10-12 19:30:10 +0200

    [175150462] Added small app to test access to NFC stack via package react-native-nfc-manager

---
+++ android/app/src/main/java/com/cie_std_pkg/MainActivity.java
@@ -0,0 +1,15 @@
+package com.cie_std_pkg;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "cie_std_pkg";
+  }
+}
