4e46d58fbeae3390ea24e2bdb177406d76686477
Matteo Boschi <mbmaken@gmail.com>
2021-07-20 13:12:50 +0200

    update

--- android/src/main/java/it/ipzs/cieidsdk/common/CieIDSdk.kt
+++ android/src/main/java/it/ipzs/cieidsdk/common/CieIDSdk.kt
@@ -150,8 +150,7 @@ object CieIDSdk : NfcAdapter.ReaderCallback {
             val isoDep = IsoDep.get(tag)
             isoDep.timeout = isoDepTimeout
             isoDep.connect()
-
-            CieIDSdkLogger.log("isExtendedLengthApduSupported: ${isoDep.isExtendedLengthApduSupported}")
+            
             if(!isoDep.isExtendedLengthApduSupported) {
                 CieIDSdkLogger.log("isExtendedLengthApduSupported : false")
                 callback?.onEvent(Event(EventSmartphone.EXTENDED_APDU_NOT_SUPPORTED))
