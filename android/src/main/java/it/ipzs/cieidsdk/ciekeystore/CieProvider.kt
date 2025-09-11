9f9da340e6a959df1832375a4ec3fcaa7b7f9efc
Fabio Bombardi <16268789+shadowsheep1@users.noreply.github.com>
2025-09-11 10:33:03 +0200

    fix: javax.net.ssl.SSLHandshakeException on different SDK provider

--- android/src/main/java/it/ipzs/cieidsdk/ciekeystore/CieProvider.kt
+++ android/src/main/java/it/ipzs/cieidsdk/ciekeystore/CieProvider.kt
@@ -3,14 +3,15 @@ package it.ipzs.cieidsdk.ciekeystore
 import java.security.Provider
 
 
-internal class CieProvider : Provider(CieProvider::class.java.simpleName, 1.0, "Provider per cie") {
+internal class CieProvider : Provider(PROVIDER_NAME, 1.0, "Provider per cie") {
 
     companion object {
-        const val PROVIDER = "CIE"
+      const val PROVIDER_NAME = "CieProvider"
+      const val KEYSTORE_TYPE = "CIE"
     }
 
     init {
-        put("KeyStore.$PROVIDER", CieKeyStore::class.java.name)
+        put("KeyStore.$KEYSTORE_TYPE", CieKeyStore::class.java.name)
         if (android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.Q) {
             put("Signature.NONEwithRSA", CieSignatureImpl.None::class.java.name)
         } else {
